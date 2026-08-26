aa2dc560c6d8669eb61294706b293958baa64540
Oksana Goncharova <oksanagoncharova2011@gmail.com>
2026-08-27 00:04:39 +0800

    hghf

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,62 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
+    var dog = 8.0;
+    var cat = 3.6 ;
+    var paper = 763789 ;
+    System.out.println( "dog" );
+    System.out.println( "cat" );
+    System.out.println("paper" );
+    System.out.println( dog );
+    System.out.println( cat );
+    System.out.println( paper );
+
+    var result = dog + 4;
+    var result1 = cat + 4 ;
+    var result2 = paper + 4 ;
+    System.out.println( result );
+    System.out.println( result1 );
+    System.out.println( result2 );
+
+    var result3 = dog - 3.5;
+    var result4 = cat - 1.6 ;
+    var result5 = paper - 7639 ;
+    System.out.println( result3 );
+    System.out.println( result4 );
+    System.out.println( result5 );
+
+    var friend = 19 ;
+    var result6 = ( friend + 2 ) / 7 ;
+    System.out.println( result6 );
+
+    var frog = 3.5 ;
+    var result7 = ( frog * 10 ) / 3.5 ;
+    var result8 = result7 + 4 ;
+    System.out.println( result8 );
+
+    var bokser = 78.2 ;
+    var bokser2 = 82.7 ;
+    var result9 = bokser + bokser2 ;
+    System.out.println( " общая масса двух бойцов " + result9 + " кг! " );
+
+
+    var result10 = bokser2 - bokser ;
+    System.out.println( " разниц между массами бойцов " + result10 + " кг! " );
+
+    var result11 = bokser2 % bokser ;
+    System.out.println( " остаток " + result11 + " кг! " );
+
+    var allHour = 640 ;
+    var hour = 8 ;
+    var result12 = allHour / hour ;
+    System.out.println( " Всего работников в компании — " + result12 + " человек " );
+
+    var man = 94 ;
+    var result13 = allHour / man ;
+    System.out.println( " Если в компании работает " + man + " человек, то всего "+ result13 + " часов работы может быть поделено между сотрудниками. " );
+
+
+
 }
+
+
