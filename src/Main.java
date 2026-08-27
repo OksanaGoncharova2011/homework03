80f409ef6304798f28ac1a4cd13f5176fdcbf6ee
Oksana Goncharova <oksanagoncharova2011@gmail.com>
2026-08-27 23:24:50 +0800

    changes in syntax

--- src/Main.java
+++ src/Main.java
@@ -18,31 +18,39 @@ void main() {
     System.out.println( result );
     System.out.println( result1 );
     System.out.println( result2 );
-//001
-    var result3 = dog - 3.5;
-    var result4 = cat - 1.6 ;
-    var result5 = paper - 7639 ;
+
+    var result3 = result - 3.5;
+    var result4 = result1 - 1.6 ;
+    var result5 = result2 - 7639 ;
     System.out.println( result3 );
     System.out.println( result4 );
     System.out.println( result5 );
 
+
     var friend = 19 ;
-    var result6 = ( friend + 2 ) / 7 ;
+    var result6 = friend + 2 ;
+    var result7 = result6  / 7 ;
+    System.out.println( friend );
     System.out.println( result6 );
+    System.out.println( result7 );
 
     var frog = 3.5 ;
-    var result7 = ( frog * 10 ) / 3.5 ;
-    var result8 = result7 + 4 ;
+    var result8 =  frog * 10 ;
+    var result9 = result8 / 3.5 ;
+    var result10 = result9 + 4 ;
+    System.out.println( frog );
     System.out.println( result8 );
+    System.out.println( result9 );
+    System.out.println( result10 );
 
     var bokser = 78.2 ;
     var bokser2 = 82.7 ;
-    var result9 = bokser + bokser2 ;
-    System.out.println( " общая масса двух бойцов " + result9 + " кг! " );
+    var resultBokser = bokser + bokser2 ;
+    System.out.println( " общая масса двух бойцов " + resultBokser + " кг! " );
 
 
-    var result10 = bokser2 - bokser ;
-    System.out.println( " разниц между массами бойцов " + result10 + " кг! " );
+    var resultBokser2 = bokser2 - bokser ;
+    System.out.println( " разниц между массами бойцов " + resultBokser2 + " кг! " );
 
     var result11 = bokser2 % bokser ;
     System.out.println( " остаток " + result11 + " кг! " );
@@ -52,8 +60,8 @@ void main() {
     var result12 = allHour / hour ;
     System.out.println( " Всего работников в компании — " + result12 + " человек " );
 
-    var man = 94 ;
-    var result13 = allHour / man ;
+    var man = result12 + 94 ;
+    var result13 = man * hour ;
     System.out.println( " Если в компании работает " + man + " человек, то всего "+ result13 + " часов работы может быть поделено между сотрудниками. " );
 
 
